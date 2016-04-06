d5c07ad5b6a0fad67a09e1043909bed0ea83e8d1
Michael P. Nitowski <mpnitowski@gmail.com>
2016-04-06 10:53:27 -0400

    Enhanced color support

--- colorize.swift
+++ colorize.swift
@@ -6,12 +6,28 @@ public enum ConsoleColor: Int
 
 public extension String {
     
+    private func escape(s: String) -> String {
+        return "\u{001B}[0;\(s)m"
+    }
+    
+    private func escape(x: Int) -> String {
+        return escape(String(x))
+    }
+
     private func escape(color: ConsoleColor) -> String {
-        return "\u{001B}[0;\(color.rawValue)m"
+        return escape(color.rawValue)
     }
 
     public func colorize(color: ConsoleColor) -> String {
         return escape(color) + self + escape(ConsoleColor.Default)
+    }
+
+    public func colorize(r : Int, g : Int, b: Int) -> String {
+        return escape("38;2;\(r);\(g);\(b)") + self + escape(ConsoleColor.Default)
+    }
+
+    public func colorize(index: Int) -> String {
+        return escape("38;5;\(index)") + self + escape(ConsoleColor.Default)
     }
 
     public func inRed() -> String { return colorize(ConsoleColor.Red) }
@@ -22,4 +38,3 @@ public extension String {
     public func inCyan() -> String { return colorize(ConsoleColor.Cyan) }
     public func White() -> String { return colorize(ConsoleColor.White) }
 }
-
--- main.swift
+++ main.swift
@@ -1,2 +1,2 @@
 print("Red".inRed() + "Green".inGreen() + "Blue".inBlue())
-
+print("color".colorize(100, g: 32, b: 45))
