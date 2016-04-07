d8edddceb02e2d2aa39768b3f95ecb5aa9bd7762
Michael P. Nitowski <mpnitowski@gmail.com>
2016-04-07 16:31:16 -0400

    Main update

--- main.swift
+++ main.swift
@@ -1,6 +1,9 @@
-var libName = "C".colorize(Color.Red) + "o".colorize(Color.BrightGreen)
+var libName = ""
+libName += "C".colorize(Color.Black).highlight(Color.White)
+libName += "o".colorize(Color.Red).highlight(Color.BrightBlue)
 libName += "l".colorize(59, g: 193, b: 237).highlight(RGB(r: 139, g: 22, b: 145))
-libName += "o".highlight(30) + "r".colorize(234)
+libName += "o".highlight(30)
+libName += "r".colorize(23)
 libName += "i".highlight(Color.Black)
 libName += "z".highlight(Color.Magenta).colorize(RGB(r: 245, g: 101, b: 5))
 libName += "e".highlight(245, g: 5, b: 93)
